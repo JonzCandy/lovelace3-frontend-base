a6356205c72dcb1c271942c3533e8e3b85b8ad7b
JonzCandy <joncandy2021@gmail.com>
2022-03-25 00:30:12 -0300

    tudo ok e feio

--- src/aboutme/script.js
+++ src/aboutme/script.js
@@ -1,1 +1,29 @@
-const $ = require( "jquery" )( window );
+const apiUrl = "https://rickandmortyapi.com/api";
+
+async function getCharacterById(num) {
+  let recebe = await fetch(`${apiUrl}/character/${num}`);
+  let pacote = await recebe.json();
+  let nome = pacote.name;
+  let resposta = await fetch(`${apiUrl}/character/?name=${nome}`);
+  let caixa = await resposta.json();
+
+  let infos = caixa.results.map((el) => {
+    return `<img src="${el.image}"/>
+            <a class="image-link" href="${el.image}">
+              Image link: ${el.image}
+            </a>
+            <div>
+              id: ${el.id}
+            </div>
+            <div>
+              name: ${el.name}
+            </div>`;
+  });
+
+  $("#pesquisado").append(infos);
+}
+
+$(document).on('click', "#recebechid",function(){
+  let ident = $("#chid").val();
+  getCharacterById(ident);
+});
